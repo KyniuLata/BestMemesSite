0f3bc1c06ed5b9cdbd8a42dce0586dacee5cc259
KyniuLata <krzysztof.lotawsb@gmail.com>
2022-05-14 13:49:33 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
+import Hottest from "./pages/Hottest.js";
+import AddMems from "./pages/AddMems.js";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="hottest" element={<Hottest />} />
+        <Route path="addMemes" element={<AddMems />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
