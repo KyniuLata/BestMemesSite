959fcd10d8b99eb122d4e2e14eced9305bf3f44f
KyniuLata <krzysztof.lotawsb@gmail.com>
2022-05-31 23:37:39 +0200

    fix header & refactor

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { incrementLikes, decrementLikes } from "./store/memes";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsDown, faThumbsUp } from "@fortawesome/fontawesome-free-solid";
+import {
+  faThumbsDown,
+  faThumbsUp,
+  faList,
+} from "@fortawesome/fontawesome-free-solid";
 import troll from "../src/assets/images/troll.png";
 import "./App.css";
 
@@ -19,13 +23,19 @@ export const MemList = (props) => {
       <div></div>
       <div className="memesScore">
         <div className="vote">
-          <div onClick={() => dispatch(incrementLikes(index))}>
+          <div
+            className="pointer"
+            onClick={() => dispatch(incrementLikes(index))}
+          >
             <FontAwesomeIcon icon={faThumbsUp} size="2x" color="green" />
           </div>
           <p className="votesCounter">{mem.likes}</p>
         </div>
         <div className="vote">
-          <div onClick={() => dispatch(decrementLikes(index))}>
+          <div
+            className="pointer"
+            onClick={() => dispatch(decrementLikes(index))}
+          >
             <FontAwesomeIcon icon={faThumbsDown} size="2x" color="red" />
           </div>
           <p className="votesCounter">{mem.dislikes}</p>
@@ -37,16 +47,22 @@ export const MemList = (props) => {
 };
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggling = () => setIsOpen(!isOpen);
   return (
-    <div className="App-header">
-      <div className="siteHeader">
-        <img
-          className="App-logo"
-          src={troll}
-          alt="memes site img"
-          height={"80vh"}
-        />
-        <div>Longest Memes Page Name Ever </div>
+    <div className="siteHeader">
+      <img
+        className="logoHeader"
+        src={troll}
+        alt="memes site img"
+        height={"80vh"}
+      />
+      <div className="headerText">Longest Memes Page Name Ever </div>
+      <div className="menuList">
+        <div onClick={toggling}>
+          <FontAwesomeIcon icon={faList} size="2x" color="white" />
+        </div>
+        {isOpen && <div>Not working yet</div>}
       </div>
     </div>
   );
