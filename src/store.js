7896e8c0ed6db0b1054105464c99a9e7dae416c7
KyniuLata <krzysztof.lotawsb@gmail.com>
2022-06-05 10:54:32 +0200

    Updates

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import memesSlice from "../src/store/memes";
-
-export default configureStore({
-  reducer: {
-    memesStore: memesSlice,
-  },
-});
